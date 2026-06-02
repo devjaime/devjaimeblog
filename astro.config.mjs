702578079a1f41d5705b95a32dd6c49eb7831b0f
Jaime Hernandez <jahernandezsa@falabella.cl>
2026-06-02 13:48:03 -0400

    feat(seo): add sitemap, json-ld, fix lang to es, fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://devjaime.com",
+  site: "https://jaimehernandez.dev",
   trailingSlash: "never",
   output: 'server',
   adapter: netlify(),
   build: {
     inlineStylesheets: "auto",
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   vite: {
     build: {
       rollupOptions: {
